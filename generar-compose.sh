ffa1acb335d15060ed7ab9041b134e01f3829a40
Juan Munoz <juanmunoz890@gmail.com>
2025-08-25 21:25:09 -0300

    impl ej1

---
+++ generar-compose.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Validación de argumentos de entrada
+if [ $# -lt 2 ]; then
+  echo "Uso: $0 <archivo_salida> <cantidad_clientes>"
+  exit 1
+fi
+
+OUTPUT_FILE="$1"
+CLIENT_COUNT="$2"
+
+cat > "$OUTPUT_FILE" <<EOL
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOL
+
+# Agregacion dinamica de clientes
+for ((i=1; i<=CLIENT_COUNT; i++)); do
+cat >> "$OUTPUT_FILE" <<EOL
+
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOL
+done
+
+
+cat >> "$OUTPUT_FILE" <<EOL
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOL
+
+echo "Archivo '$OUTPUT_FILE' creado exitosamente con $CLIENT_COUNT clientes."
